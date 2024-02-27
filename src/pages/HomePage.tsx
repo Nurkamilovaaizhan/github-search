8923b82e3d68641593ad4dd2ccdad26465ab96f5
Nurkamilovaaizhan <146005300+Nurkamilovaaizhan@users.noreply.github.com>
2024-02-28 02:24:40 +0600

    without a scroll

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -23,7 +23,7 @@ export function HomePage() {
   }
 
   return (
-    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+    <div className="flex justify-center pt-10 mx-auto ">
       { isError && <p className="text-center text-red-600">Something went wrong...</p> }
 
       <div className="relative w-[560px]">
